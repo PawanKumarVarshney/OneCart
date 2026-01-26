901eee91670a7d0cde740117848f8b8283ff290a
Pawan Kumar Varshney <pawanvarshney38@gmail.com>
2026-01-26 19:38:06 +0530

    Update server URL in AuthContext

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createContext } from 'react'
 export const authDataContext= createContext()
 function AuthContext({children}) {
-    let serverUrl = "https://localhost:8000"
+    let serverUrl = "https://shop-backend-x06v.onrender.com"
 
     let value = {
        serverUrl
