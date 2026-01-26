d05e82eab0f3ee85e19116b9eaf7d4e6209c79a2
Pawan Kumar Varshney <pawanvarshney38@gmail.com>
2026-01-27 00:12:33 +0530

    Update AuthContext.jsx

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createContext } from 'react'
 export const authDataContext= createContext()
 function AuthContext({children}) {
-    let serverUrl = "https://shop-backend-x06v.onrender.com"
+    let serverUrl = "http://localhost:8000"
 
     let value = {
        serverUrl
